0cb0b45b3872d64ac9eb83234abcf0d12cf25b14
Matt Brennan <mattyb1000@gmail.com>
2016-06-05 12:50:00 +0100

    splash page and some other stuf

---
+++ client/main.jsx
@@ -0,0 +1,25 @@
+import {Meteor} from 'meteor/meteor';
+import React from 'react';
+import {render} from 'react-dom';
+import Blaze from 'meteor/gadicc:blaze-react-component';
+import {createContainer} from 'meteor/react-meteor-data';
+
+const Account = (props) => <Blaze template="loginButtons" {...props} />;
+
+const Logo = () => <a href="/" className="logo">muerte.club</a>;
+
+const IntroPage = () => <div>
+  <Logo />
+  <div className="tagline">Movie nights with a random twist</div>
+  <Account />
+</div>;
+
+const App = ({loggedIn}) => (loggedIn ? <Account /> : <IntroPage />);
+
+const AppContainer = createContainer(() => ({
+  loggedIn: Meteor.user(),
+}), App);
+
+Meteor.startup(() => {
+  render(<AppContainer />, document.querySelector('main'));
+});
