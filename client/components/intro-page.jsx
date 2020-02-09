81cfac866f422e57489d297fae3f5b0c26f39061
bren <bren@153.io>
2020-02-09 11:47:20 +0000

    add a tagline

--- client/components/intro-page.jsx
+++ client/components/intro-page.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import { Grid } from 'semantic-ui-react'
+import { Grid, Header } from 'semantic-ui-react'
 import Logo from './logo'
 import Account from './account'
 
 const IntroPage = () => (
 	<Grid centered verticalAlign='middle' className='fullheight'>
 		<Grid.Column width={8}>
-			<Logo />
+			<Header>
+				<Logo />
+			</Header>
+			<Header as='h2'>Your bucket list of movies.</Header>
+			<p>
+				Tell us the movies you want to see before you die, get a group together,
+				and we'll decide what's showing at your movie night.
+			</p>
 			<Account />
 		</Grid.Column>
 	</Grid>
